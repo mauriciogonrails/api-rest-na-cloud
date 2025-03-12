8985b39329a9e97545aeb0dbd6d7bb037bf480da
DevMau <141933693+mauriciogonrails@users.noreply.github.com>
2025-03-12 11:00:40 -0300

    Update DesafioapiApplicationTests.java

--- API-Rest/src/test/java/com/github/mauriciogomes/api/DesafioapiApplicationTests.java
+++ API-Rest/src/test/java/com/github/mauriciogomes/api/DesafioapiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.wesleyav.desafioapi;
+package com.github.mauriciogomes.apirestnacloud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
